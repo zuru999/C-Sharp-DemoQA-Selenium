e89840456290cb069e877fd936591c1f6062f6b4
zuru999 <zuru.mati@gmail.com>
2016-10-28 10:28:41 +0200

    Update SeleniumGet/SetMethods

---
+++ DemoQA-WebTest/SeleniumGetMethods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace DemoQA_WebTest
+{
+    class SeleniumGetMethods
+    {
+        
+        public static string GetTextFromDropDown(string element, PropertyType elementtype)
+        {
+            if (elementtype == PropertyType.Id)
+                return new SelectElement(PropertiesToChromeDriver.chrome.FindElement(By.Id(element))).AllSelectedOptions.SingleOrDefault().Text;
+            if (elementtype == PropertyType.Name)
+                return new SelectElement(PropertiesToChromeDriver.chrome.FindElement(By.Name(element))).AllSelectedOptions.SingleOrDefault().Text;
+            else return String.Empty;
+        }
+
+    }
+}
---
+++ DemoQA-WebTest/SeleniumSetMethods.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+
+namespace DemoQA_WebTest
+{
+    class SeleniumSetMethods
+    {
+        // Selecting a dropdown control
+
+        public static void SelectDropDown (string element, string value, PropertyType elementtype)
+        {
+            if (elementtype == PropertyType.Id)
+                new SelectElement(PropertiesToChromeDriver.chrome.FindElement(By.Id(element))).SelectByText(value);
+            if (elementtype == PropertyType.Name)
+                new SelectElement(PropertiesToChromeDriver.chrome.FindElement(By.Name(element))).SelectByText(value);
+
+        }
+    }
+}
